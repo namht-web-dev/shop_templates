cc7b158cf5913522164464e627c4f851171fc259
namht-web-dev <hocdieuhay99@gmail.com>
2026-09-08 16:31:06 +0700

    update

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import type { MetadataRoute } from "next";
+import { siteConfig } from "@/src/config/site";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = siteConfig.url;
+
+  return [
+    {
+      url: `${baseUrl}/vi`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/en`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1.0,
+    },
+  ];
+}
