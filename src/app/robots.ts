ff87b4379fb4b4b5832e6948a520e429ef933805
namht-web-dev <hocdieuhay99@gmail.com>
2026-09-08 16:28:19 +0700

    update

---
+++ src/app/robots.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from "next";
+import { siteConfig } from "@/src/config/site"; // Hoặc đường dẫn file config URL của bạn
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/account/", "/api/"], // Các trang bạn không muốn Google cào dữ liệu
+    },
+    // Thay đổi domain thật của bạn ở đây:
+    sitemap: `${siteConfig.url}/sitemap.xml`,
+    // Hoặc viết trực tiếp: 'https://domain-cua-ban.com/sitemap.xml'
+  };
+}
